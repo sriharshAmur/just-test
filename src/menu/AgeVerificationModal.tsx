bfcb26c3206a1477bf353b2c26b941b3e77a4c0a
sriharshAmur <sriharshamur@gmail.com>
2024-11-18 11:32:56 +0100

    Fixed TS Issues 2

--- src/menu/AgeVerificationModal.tsx
+++ src/menu/AgeVerificationModal.tsx
@@ -1,12 +1,11 @@
-import React from "react";
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogContent,
   DialogDescription,
+  DialogFooter,
   DialogHeader,
   DialogTitle,
-  DialogFooter,
 } from "@/components/ui/dialog";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Separator } from "@/components/ui/separator";
