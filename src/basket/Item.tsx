7030cf35a0713bbd15b7c6e90f94516876820e45
Sriharsh Amur <sriharshamur@gmail.com>
2024-11-18 10:31:17 +0100

    Added Modals - Item Selector and Age Verifications (#1)
    * Modal commits
    
    * Added Data QA Tags, Added age verification modal (Commented out)

--- src/basket/Item.tsx
+++ src/basket/Item.tsx
@@ -10,25 +10,89 @@ const Item = ({ item }: Props) => {
   const { addItem, removeItem } = useMenuBasketStore();
 
   return (
-    <div className="flex flex-col gap-2">
-      <div className="flex items-start justify-between gap-1 px-2">
-        <div className="flex-1 break-words font-bold underline">
+    <div
+      className="flex flex-col gap-2"
+      data-qa={`basket-item-${item.id}`}
+    >
+      <div
+        className="flex items-start justify-between px-2"
+        data-qa="basket-item-header"
+      >
+        <div
+          className="font-bold underline"
+          data-qa="basket-item-name"
+        >
           {item.name}
         </div>
-        <div className="text-right">€{item.price}</div>
+        <div className="text-right">
+          <div data-qa="basket-item-price">€{item.price}</div>
+        </div>
       </div>
-      <div>{/* addons */}</div>
-      <div className="ml-auto flex w-fit select-none items-center gap-4 rounded-2xl bg-gray-200 px-2 py-1">
+      <div
+        className="pl-2"
+        data-qa="basket-item-details"
+      >
+        {item.variation && (
+          <div
+            className="text-sm text-gray-700"
+            data-qa="basket-item-variation"
+          >
+            <div className="font-semibold">Variation:</div>
+
+            <div className="flex items-center justify-between">
+              <div>{item.variation.name}</div>
+              <div>(+€{item.variation.price})</div>
+            </div>
+          </div>
+        )}
+        {item?.selectedOptions?.length > 0 && (
+          <div
+            className="mt-1 text-sm text-gray-700"
+            data-qa="basket-item-options"
+          >
+            <span className="font-semibold">Options:</span>
+            <ul className="list-inside list-disc">
+              {item.selectedOptions!.map((selectedOption) => {
+                const group = item.optionGroups.find(
+                  (g) => g.id === selectedOption.groupId,
+                );
+                const option = group?.options.find(
+                  (o) => o.id.toString() === selectedOption.optionId,
+                );
+
+                return option ? (
+                  <li
+                    key={`${group.id}-${option.id}`}
+                    className="flex justify-between"
+                    data-qa={`basket-item-option-${group.id}-${option.id}`}
+                  >
+                    <span>{option.name}</span>
+                    {option.price !== undefined && (
+                      <span className="text-gray-500">(+€{option.price})</span>
+                    )}
+                  </li>
+                ) : null;
+              })}
+            </ul>
+          </div>
+        )}
+      </div>
+      <div
+        className="ml-auto flex items-center gap-4 rounded-2xl bg-gray-200 px-2 py-1"
+        data-qa="basket-item-quantity-controls"
+      >
         <div
           className="cursor-pointer"
           onClick={() => removeItem(item)}
+          data-qa="basket-item-decrease-button"
         >
           {item.quantity > 1 ? <Minus /> : <Trash size={17} />}
         </div>
-        <div>{item.quantity}</div>
+        <div data-qa="basket-item-quantity">{item.quantity}</div>
         <div
           className="cursor-pointer"
           onClick={() => addItem(item)}
+          data-qa="basket-item-increase-button"
         >
           <Plus size={17} />
         </div>
--- src/menu/ItemCard.tsx
+++ src/menu/ItemCard.tsx
@@ -1,53 +1,134 @@
 import { cn } from "@/lib/utils";
 import { useMenuBasketStore } from "@/store/menuBasketStore";
-import { MenuItem } from "@/types";
+import { AddBasketItem, MenuItem } from "@/types";
 import { Plus } from "lucide-react";
+import { useState } from "react";
+import ItemSelector from "./ItemSelector";
+import AgeVerificationModal from "./AgeVerificationModal";
 
-type Props = {
+interface ItemCardProps {
   item: MenuItem;
-};
+}
 
-const ItemCard = ({ item }: Props) => {
+const ItemCard = ({ item }: ItemCardProps) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isAgeVerificationModalOpen, setIsAgeVerificationModalOpen] =
+    useState(false);
   const { addItem } = useMenuBasketStore();
 
+  const handleQuickAdd = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    if (item.type === "simple") {
+      addItem(item);
+    } else {
+      setIsModalOpen(true);
+    }
+  };
+
+  const handleAddToBasket = (item: AddBasketItem) => {
+    addItem(item);
+    setIsModalOpen(false);
+  };
+
+  const handleItemClick = () => {
+    // setIsAgeVerificationModalOpen(true);
+    setIsModalOpen(true);
+  };
+
+  const handleAgeVerification = (isAbove18: boolean) => {
+    if (isAbove18) {
+      setIsModalOpen(true);
+    } else {
+      console.log("User is under 18. Access denied.");
+    }
+  };
+
   return (
-    <div className="relative flex cursor-pointer items-center rounded-lg border border-gray-300 px-4 py-4 hover:bg-gray-100">
-      <div className="flex flex-1 flex-col gap-1.5">
-        <div>
-          <h3 className="font-bold">{item.name}</h3>
-          <p className="text-sm font-semibold">€ {item.price}</p>
+    <>
+      <div
+        className="relative flex cursor-pointer items-center rounded-lg border border-gray-300 px-4 py-4 hover:bg-gray-100"
+        onClick={handleItemClick}
+        data-qa="item-card"
+      >
+        <div
+          className="flex flex-1 flex-col gap-1.5"
+          data-qa="item-details"
+        >
+          <div>
+            <h3
+              className="font-bold"
+              data-qa="item-name"
+            >
+              {item.name}
+            </h3>
+            <p
+              className="text-sm font-semibold"
+              data-qa="item-price"
+            >
+              €{item.price.toFixed(2)}
+            </p>
+          </div>
+          {item.description && (
+            <p
+              className="text-sm text-gray-700"
+              data-qa="item-description"
+            >
+              {item.description}
+            </p>
+          )}
+          {item.ingredients && (
+            <p
+              className="text-xs text-gray-600"
+              data-qa="item-ingredients"
+            >
+              {item.ingredients}
+            </p>
+          )}
         </div>
-        {item.description && (
-          <p className="text-sm text-gray-700">{item.description}</p>
-        )}
-        {item.ingredients && (
-          <p className="text-xs text-gray-600">{item.ingredients}</p>
-        )}
+
+        <div
+          className="relative h-24 w-24 rounded-md bg-gray-300"
+          data-qa="item-image"
+        >
+          <QuickAddIcon
+            positionClasses="absolute -top-2 -right-2"
+            onClick={handleQuickAdd}
+          />
+        </div>
       </div>
 
-      <div onClick={() => addItem(item)}>
-        {item.id % 2 === 0 ? (
-          <div className="relative h-24 w-24 rounded-md bg-gray-300">
-            <QuickAddIcon positionClasses="absolute -top-2 -right-2" />
-          </div>
-        ) : (
-          <QuickAddIcon positionClasses="absolute top-2 right-2" />
-        )}
-      </div>
-    </div>
+      <AgeVerificationModal
+        isOpen={isAgeVerificationModalOpen}
+        onClose={() => setIsAgeVerificationModalOpen(false)}
+        onVerify={handleAgeVerification}
+      />
+
+      <ItemSelector
+        item={item}
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        onAddToBasket={handleAddToBasket}
+      />
+    </>
   );
 };
 
 type QuickAddIconProps = {
   positionClasses: string;
+  onClick: (e: React.MouseEvent) => void;
 };
 
-const QuickAddIcon = ({ positionClasses }: QuickAddIconProps) => {
+const QuickAddIcon = ({ positionClasses, onClick }: QuickAddIconProps) => {
   return (
-    <div className={cn(positionClasses)}>
+    <div
+      className={cn(positionClasses)}
+      data-qa="quick-add-icon-container"
+    >
       <button
         className="rounded-full border bg-white p-1 shadow-md hover:bg-gray-200 focus:outline-none"
         aria-label="Add item"
+        onClick={onClick}
+        data-qa="quick-add-button"
       >
         <Plus className="h-5 w-5 text-primary" />
       </button>
